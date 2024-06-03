e5fd1d68036c81e11ee63f3a51883d3eb05d7614
jmtoledo175 <juli.toledo175@gmail.com>
2024-06-02 23:20:03 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace pryCalculadoraImpuestos
+{
+    partial class fmrImpuestos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmdCalcular = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboPais = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(80, 44);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(169, 20);
+            this.txtPrecio.TabIndex = 0;
+            this.txtPrecio.TextChanged += new System.EventHandler(this.txtPrecio_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmdCalcular);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cboPais);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtPrecio);
+            this.groupBox1.Location = new System.Drawing.Point(31, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(285, 227);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos";
+            // 
+            // cmdCalcular
+            // 
+            this.cmdCalcular.Enabled = false;
+            this.cmdCalcular.Location = new System.Drawing.Point(80, 159);
+            this.cmdCalcular.Name = "cmdCalcular";
+            this.cmdCalcular.Size = new System.Drawing.Size(112, 32);
+            this.cmdCalcular.TabIndex = 4;
+            this.cmdCalcular.Text = "Calcular";
+            this.cmdCalcular.UseVisualStyleBackColor = true;
+            this.cmdCalcular.Click += new System.EventHandler(this.cmdCalcular_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pais:";
+            // 
+            // cboPais
+            // 
+            this.cboPais.FormattingEnabled = true;
+            this.cboPais.Items.AddRange(new object[] {
+            "Estados Unidos",
+            "Canadá",
+            "México",
+            "Otros"});
+            this.cboPais.Location = new System.Drawing.Point(80, 101);
+            this.cboPais.Name = "cboPais";
+            this.cboPais.Size = new System.Drawing.Size(170, 21);
+            this.cboPais.TabIndex = 2;
+            this.cboPais.SelectedIndexChanged += new System.EventHandler(this.cboPais_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Precio:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultado.Location = new System.Drawing.Point(31, 276);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(285, 82);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // fmrImpuestos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 389);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "fmrImpuestos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculador de impuestos";
+            this.Load += new System.EventHandler(this.fmrImpuestos_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboPais;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cmdCalcular;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryCalculadoraImpuestos
+{
+    public partial class fmrImpuestos : Form
+    {
+        public fmrImpuestos()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fmrImpuestos_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void cmdCalcular_Click(object sender, EventArgs e)
+        {
+            
+            decimal precio = Convert.ToDecimal(txtPrecio.Text);
+
+            if (cboPais.SelectedItem != null)
+            {
+               
+                string pais = cboPais.SelectedItem.ToString();
+
+                switch (pais)
+                {
+                    case "Estados Unidos":
+                        precio = precio +(precio * 0.07m) ;
+                        break;
+                    case "Canadá":
+                        precio = precio + (precio * 0.05m);
+                        break;
+                    case "México":
+                        precio = precio + (precio * 0.16m);
+                        break;
+                    case "Otros":
+                        precio = precio + (precio * 0.10m);
+                        break;
+                }
+                lblResultado.Text = $"El precio con IVA es: {precio:C}";
+
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un Pais");
+            }
+
+        }
+
+        private void cboPais_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPrecio_TextChanged(object sender, EventArgs e)
+        {
+            if(txtPrecio.Text == "")
+            {
+                cmdCalcular.Enabled = false;
+            }
+            else
+            {
+                cmdCalcular.Enabled = true;
+            }
+        }
+    } 
+}
